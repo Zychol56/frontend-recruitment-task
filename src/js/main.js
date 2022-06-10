719458fd5133ced0af7e526964cf30473f86b5dd
Zychol56 <zychol56@gmail.com>
2022-06-10 12:22:12 +0200

    Finish
    Przepraszam za brak commitów ale zadanie było na tyle krótkie że się zapędziłem.

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,43 @@
-window.addEventListener('DOMContentLoaded', (e) => {
+window.addEventListener('DOMContentLoaded', () => {
 
     const popupContainer = document.querySelector(".popupContainer");
-    const popupBox = document.querySelector(".popupBox");
-    const Bttn = document.querySelector(".recruitment-module a");
+    const popupBackground = document.querySelector(".popupBackground");
+    const contentBttn = document.querySelector(".recruitment-module button");
+    const closeBttn = document.querySelector(".closeBtn");
+    const popupBoxP = document.querySelector(".popupBox p");
+    const resetBttn = document.querySelector(".resetBttn");
+    
+    const OpenClose = Bttn => {
+        Bttn.addEventListener("click", () => {
+            if (!popupContainer.classList.contains("show")) {
+                popupContainer.classList.add("show");
+                if((JSON.parse(localStorage.getItem('Count'))).Count >= 5){
+                    resetBttn.classList.add("show");
+                }
+            } else {
+                popupContainer.classList.remove("show"); 
+                resetBttn.classList.remove("show");
+            }
+            if(Bttn === contentBttn){
+                localStorage.setItem('Count', JSON.stringify({
+                    Count: (JSON.parse(localStorage.getItem('Count'))).Count + 1 || 0
+                  }));
+                popupBoxP.innerHTML = 'You have clicked <strong>' + (JSON.parse(localStorage.getItem('Count'))).Count + ' times </strong>to related button.';;
+            }
 
-    Bttn.addEventListener("click", () => {
-          if (!popupContainer.classList.contains("show")) {
-            popupContainer.classList.add("show");
-          } else {
-            popupContainer.classList.remove("show"); 
-          }
-      });
+        });
+    }
+
+    OpenClose(closeBttn)
+    OpenClose(contentBttn)
+    OpenClose(popupBackground)
+
+    resetBttn.addEventListener("click", () => {
+        localStorage.setItem('Count', JSON.stringify({
+            Count: 0
+        }));
+        resetBttn.classList.remove("show");
+        popupBoxP.innerHTML = 'You have clicked <strong>' + (JSON.parse(localStorage.getItem('Count'))).Count + ' times </strong>to related button.';;
+    });
 
 });
